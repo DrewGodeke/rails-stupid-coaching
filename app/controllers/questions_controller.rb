a938dde420dcba9542713156f9788e0df51b75e9
andrew godeke <legodeke@hotmail.com>
2024-04-06 12:31:32 -0400

    Coach's answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,18 @@
 class QuestionsController < ApplicationController
-  def ask
+  def answer
+    question = params[:question]
+    @answer = coach_answer(question)
+  end
+
+  private
+
+  def coach_answer(question)
+    if question == "I am going to work"
+      "Great!"
+    elsif question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
